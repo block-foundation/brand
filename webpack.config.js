ccd6dfbab9ec31aa40d31151e857f6b00ee9c743
Lars van Vianen <lars@vianen.com>
2023-08-05 17:36:27 +0200

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const cleanPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode:'development',
+    entry:'./src/app/main.ts',
+    output:{
+        filename:'bundle.js',
+        path:path.resolve(__dirname,'dist')
+    },
+    devtool:'inline-source-map',
+    module:{
+        rules:[
+            {
+                test:/\.ts$/,
+                use: 'ts-loader',
+                exclude:/node_modules/
+            }
+        ]
+    },
+    resolve:{
+        extensions:['.ts','.js']
+    },
+    devServer: {
+        liveReload: true,
+        // disableHostCheck: true,
+        static: {
+            directory: path.join(__dirname, 'dist'),
+            publicPath: '/',
+        },
+        devMiddleware: {
+            writeToDisk: true,
+        },
+    },
+    plugins: [
+        new cleanPlugin.CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public' }
+            ]
+        })
+    ]
+};
